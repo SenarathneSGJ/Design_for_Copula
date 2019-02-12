aad722c66a93bca8d5f47f4d1a9a44ac3ed65adb
Jagath Senarathne Siththara Gedara <n9555293@qut.edu.au>
2019-02-12 14:23:35 +1000

    main com1

---
+++ Main_script.R
@@ -0,0 +1,13 @@
+
+#Example1 Setup1 (When data are generating from Frank Copula)
+setwd("./Example1_Setup1")
+source("mainBF_FrankT.R")
+
+setwd("./Example1_Setup2/Model1")
+source("mainBF_FrankT.R")
+
+setwd("./Example1_Setup2/Model2")
+source("mainBF_FrankT.R")
+
+setwd("./Example2/Frank_m1")
+source("mainBF_FrankT.R")
